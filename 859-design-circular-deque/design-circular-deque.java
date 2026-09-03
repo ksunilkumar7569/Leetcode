589fcd66b645630c13f7afd46745b4cd6c4d7102
K.Sunil Kumar <kuttalasunilkumar7569@gmail.com>
2026-09-03 11:50:58 +0530

    Time: 5 ms (85.37%) | Memory: 46.1 MB (95.86%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.java
@@ -0,0 +1,75 @@
+class MyCircularDeque {
+    private final int[] arr;
+    private final int capacity;
+    private int front;
+    private int rear;
+
+    public MyCircularDeque(int k) {
+        capacity = k + 1;
+        arr = new int[capacity];
+        front = 0;
+        rear = 0;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        front = (front - 1 + capacity) % capacity;
+        arr[front] = value;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        arr[rear] = value;
+        rear = (rear + 1) % capacity;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        front = (front + 1) % capacity;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        rear = (rear - 1 + capacity) % capacity;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[(rear - 1 + capacity) % capacity];
+    }
+
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % capacity == front;
+    }
+}
